8370d77a09492311fde21efa7e9cf9b4c7e7c38f
ritahsiao <ritahsiao32@gmail.com>
2023-05-17 11:22:16 +0800

    更新week14_挑選關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -6,13 +6,18 @@ GLMmodel * head =NULL;///week13 step02-1
 GLMmodel * body =NULL;
 GLMmodel * uparmR =NULL;
 GLMmodel * lowarmR =NULL;
-int show[4]={0,1,0,0};
+int show[4]={1,1,1,1};///week14_step02-2
+int ID=0;///0:頭 1:身體 2:上手臂 3:下手臂///week14_step02-2
 void keyboard(unsigned char key,int x ,int y)
 {
-    if(key=='0') show[0] = !show[0];///!是將1變0,0變1
-    if(key=='1') show[1] = !show[1];
-    if(key=='2') show[2] = !show[2];
-    if(key=='3') show[3] = !show[3];
+    if(key=='0') ID = 0;///week14_step02-2
+    if(key=='1') ID = 1;///week14_step02-2
+    if(key=='2') ID = 2;///week14_step02-2
+    if(key=='3') ID = 3;///week14_step02-2
+    ///if(key=='0') show[0] = !show[0];///!是將1變0,0變1
+    ///if(key=='1') show[1] = !show[1];
+    ///if(key=='2') show[2] = !show[2];
+    ///if(key=='3') show[3] = !show[3];
     glutPostRedisplay();
 }///將原本的keyboard函式先註解掉
 FILE * fout = NULL;///一開始,檔案沒有開 NULL
@@ -32,13 +37,23 @@ void display()
             lowarmR=glmReadOBJ("model/lowarmR.obj");
             ///glmUnitize(body);///這之後還會更改
         }
+        if(ID==0) glColor3f(1,0,0);///選定的,紅色
+        else glColor3f(1,1,1);///沒選定,白色
         if(show[0]) glmDraw(head,GLM_MATERIAL);
+
+        if(ID==1) glColor3f(1,0,0);///選定的,紅色
+        else glColor3f(1,1,1);///沒選定,白色
         if(show[1]) glmDraw(body,GLM_MATERIAL);
         glPushMatrix();
             glTranslatef(teapotX, teapotY, 0);
 
+            if(ID==2) glColor3f(1,0,0);///選定的,紅色
+            else glColor3f(1,1,1);///沒選定,白色
             if(show[2]) glmDraw(uparmR,GLM_MATERIAL);
         glPopMatrix();
+
+        if(ID==3) glColor3f(1,0,0);///選定的,紅色
+        else glColor3f(1,1,1);///沒選定,白色
         if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
